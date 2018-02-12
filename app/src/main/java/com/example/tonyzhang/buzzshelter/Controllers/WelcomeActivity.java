ab8a810e4b2d13e21cc15b1ce0b4cfa01dfaf9bc
tonyz0909 <tonyz0909@gmail.com>
2018-02-12 18:31:52 -0500

    new login screen
    navigation between pages finished, new login page created

---
+++ app/src/main/java/com/example/tonyzhang/buzzshelter/Controllers/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package com.example.tonyzhang.buzzshelter.Controllers;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.tonyzhang.buzzshelter.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        //for button to redirect to login screen
+        Button button = (Button) findViewById(R.id.MyButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
